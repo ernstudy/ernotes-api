fcf9b3418d178483def81db5d56b99604480e597
ernstudy <saingillese@gmail.com>
2026-05-25 10:43:59 -0400

    feat: create and configure notes controller

---
+++ controllers/notes.controller.js
@@ -0,0 +1,48 @@
+import NotesService from "../services/notes.service.js";
+
+const notesService = new NotesService();
+
+class NoteController {
+  async getAllUserNotes(req, res) {
+    const userId = req.user.id; // Assuming user ID is available in req.user
+    const notes = await notesService.findAllNotes(userId);
+
+    const allUserNotes = {
+      message: "Notes retrieved successfully",
+      notes,
+    };
+
+    res.json(allUserNotes);
+  }
+
+  async createUserNote(req, res) {
+    try {
+      const userId = req.user.id;
+      const noteData = req.body;
+
+      const newNote = await notesService.createNote(noteData, userId);
+      res.status(201).json(newNote);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async getUserNoteById(req, res) {
+    const userId = req.user.id;
+    const noteId = req.params.id;
+
+    try {
+      const note = await notesService.findOneNote(noteId, userId);
+
+      const createdNote = {
+        message: "Note retrieved successfully",
+        note,
+      };
+      res.json(createdNote);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
+  }
+}
+
+export default NoteController;
