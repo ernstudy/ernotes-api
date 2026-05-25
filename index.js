1f19f4208f45f32a8222e06dab6f03b76b900f95
ernstudy <saingillese@gmail.com>
2026-05-25 10:53:21 -0400

    fix: basic fixes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 
 import router from "./routes/index.js";
 import apiRouter from "./routes/index.js";
 
-import config from "./config/config.js";
+import config from "./config/env.config.js";
 const app = express();
+app.use(express.json());
 
 apiRouter(app);
 
