04da6a38ac3e266b24f470c2822bec64e083a826
ernstudy <saingillese@gmail.com>
2026-05-25 10:39:41 -0400

    feat: create and configure auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import supabase from "../config/supabase.config.js";
+
+const authenticateToken = async (req, res, next) => {
+  const accessToken = req.headers.authorization?.split(" ")[1];
+
+  if (!accessToken) {
+    return res.status(401).json({
+      error: "Unauthorized",
+    });
+  }
+
+  try {
+    const { data, error } = await supabase.auth.getUser(accessToken);
+
+    if (error || !data.user) {
+      return res.status(403).json({
+        error: "Invalid token",
+      });
+    }
+
+    req.user = data.user;
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+export default authenticateToken;
